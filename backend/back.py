dd823fc97f446ea1d8f93142b29371cffe8341ed
burin <burin010n@gmail.com>
2018-09-01 12:07:20 +0700

    bug

--- backend/back.py
+++ backend/back.py
@@ -1,102 +1,64 @@
 from flask import Flask,request,jsonify,render_template
 from flask_cors import CORS
-from flask_pymongo import PyMongo
+# from flask_pymongo import PyMongo
 from bson import json_util, ObjectId
 from gensim.models.doc2vec import Doc2Vec
+import pandas as pd
 import numpy as np
 import json
 import re
+from dialogflow import detect_intent_texts
+from pythainlp import word_tokenize
 
 app = Flask(__name__, template_folder="build", static_folder="build/static")
 #app.config["MONGO_URI"] = "mongodb://localhost:27017/essenseDB"
-mongo = PyMongo(app)
+# mongo = PyMongo(app)
 CORS(app)
-Projects = mongo.db.projects
+# Projects = mongo.db.projects
 dataset = pd.read_csv('ภาคเหนือ_part1.csv')
 dataset2 = pd.read_csv('ภาคเหนือ_part2.csv')
 dataset3 = pd.read_csv('ภาคเหนือ_part3.csv')
 dataset = dataset.append(dataset2)
 dataset = dataset.append(dataset3)
-Doc2Vec.load('doc2vec_model')
+model = Doc2Vec.load('doc2vec_model')
 
 def cleanText(text):
-    text = re.sub('[^ก-๙\s]','',des)
-    text = re.sub('[\n\t]','',des)
-    text = re.sub('[\s]+',' ',des)
+    text = re.sub('[^ก-๙\s]','',text)
+    text = re.sub('[\n\t]','',text)
+    text = re.sub('[\s]+',' ',text)
     return text
 
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def catch_all(path):
-    return render_template('index.html')
-
-@app.route("/project/create",methods=['POST'])
-def createProject():
+@app.route("/api/getIntention",methods=['POST'])
+def classifyText():
     data = json.loads(request.data.decode('utf-8'))
-    obj = {}
-    if (len(data['keywords']) == 0 or len(data['projName']) == 0):
-        obj['isSuccess'] = False
-        obj['errMsg'] = 'Please fill the form correctly'
-        return jsonify(obj) , 200
-    obj['isSuccess'] = True
-    obj['errMsg'] = ''
-    Projects.insert_one(data)
-    return jsonify(obj) , 200
+    text = data['text']
+    print('text',text)
+    res = detect_intent_texts(
+                project_id="ehack-fc084",
+                session_id="mosaku",
+                texts=[text],
+                language_code="th"
+                )
+    
+    intent = res.query_result.intent.display_name
+    return jsonify({'intent':intent}) , 200
+    
 
-@app.route("/project/delete",methods=['POST'])
-def delProject():
-    data = json.loads(request.data.decode('utf-8'))
-    obj = {}
-    print(data)
-    obj['fb'] = Projects.remove({'_id':ObjectId(data['_id'])})
-    return jsonify(obj) , 200
+@app.route("/api/getRecommend", methods=["POST"])
+def recommend():
+    text = json.loads(request.data.decode('utf-8'))['text']
+    
+    tokenized_words = word_tokenize(cleanText(text))
+    results = model.docvecs.most_similar([model.infer_vector(tokenized_words)])
+    
+    res = dataset.iloc[[res[0][0] for res in results]]
 
-@app.route("/project/get",methods=['GET'])
-def getProject():
-    docs =  Projects.find({})
-    obj = {}
-    obj['docs'] = json.loads(json_util.dumps(docs))
-    return jsonify(obj) , 200
+    obj = json.loads(res.to_json(orient='records', force_ascii=False))    
+    return jsonify(obj), 200
 
-@app.route("/upload/csv",methods=['POST'])
-def upload_csv():
-    file = request.files['file']
-    pid = request.form['pid']
-    obj = {}
-    if (file.filename.split('.')[1] in ['.csv']):
-        obj['isSuccess'] = True
-        obj['errMsg'] = ''
-        fs.put(file,filename=file.filename,content_type=file.content_type,pid=ObjectId(pid))
-        return jsonify(obj) , 200
-    elif (file.filename == ''):
-        obj['isSuccess'] = False
-        obj['errMsg'] = 'please put some file'
-        return jsonify(obj) , 200
-    else:
-        obj['isSuccess'] = False
-        obj['errMsg'] = 'file is not support'
-        return jsonify(obj) , 200
+# @app.route("/api/getAsking", methods=["POST"])
+# def asking():
 
-# @app.route("/predict/sentiment2",methods=['POST'])
-# def predict_sentiment2():
-#     global sentiment_ensemble_model
-#     data = json.loads(request.data)
-#     text = data['text']
-#     feature = dp.create_feature(text,200)
-#     ans = sentiment_ensemble_model.predict(feature)
-#     ans2 = product_model.predict(feature)
-#     obj = {}
-#     obj['sentiment'] = str(ans)
-#     obj['product'] = str(ans2)
-#     return jsonify(obj),201
-
-# @app.route("/dev/speech2text",methods=['POST'])
-# def speech2text():
-#     obj = request.form
-#     print('form',obj)
-#     print('file',request.files)
-#     return '',201
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- backend/dialogflow.py
+++ backend/dialogflow.py
@@ -1,5 +1,5 @@
 import dialogflow_v2 as dialogflow
-
+import json
 def detect_intent_texts(project_id, session_id, texts, language_code):
     """Returns the result of detect intent with texts as inputs.
 
@@ -20,16 +20,15 @@ def detect_intent_texts(project_id, session_id, texts, language_code):
         response = session_client.detect_intent(
             session=session, query_input=query_input)
 
-        print('=' * 20)
-        print('Query text: {}'.format(response.query_result.query_text))
-        print('Detected intent: {} (confidence: {})\n'.format(
-            response.query_result.intent.display_name,
-            response.query_result.intent_detection_confidence))
-        print('Fulfillment text: {}\n'.format(
-            response.query_result.fulfillment_text))
-
-        print(response)
-
+        # print('=' * 20)
+        # print('Query text: {}'.format(response.query_result.query_text))
+        # print('Detected intent: {} (confidence: {})\n'.format(
+        #     response.query_result.intent.display_name,
+        #     response.query_result.intent_detection_confidence))
+        # print('Fulfillment text: {}\n'.format(
+        #     response.query_result.fulfillment_text))
+        
+        return response
 
 
 detect_intent_texts(
