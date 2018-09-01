a6853c0cb44f1bb448bad3ac6ce6f704389165a9
burin <burin010n@gmail.com>
2018-09-01 14:04:13 +0700

    com

--- backend/back.py
+++ backend/back.py
@@ -20,13 +20,22 @@ dataset2 = pd.read_csv('ภาคเหนือ_part2.csv')
 dataset3 = pd.read_csv('ภาคเหนือ_part3.csv')
 dataset = dataset.append(dataset2)
 dataset = dataset.append(dataset3)
+map_media = pd.read_csv('north_media.csv')
+
+
+def getPicture(input_id):
+    results = map_media[mep_media[id] == input_id]
+    
+
+
 model = Doc2Vec.load('doc2vec_model')
 
 def cleanText(text):
-    text = re.sub('[^ก-๙\s]','',text)
+    text = re.sub('[^ก-๙\s0-9]','',text)
     text = re.sub('[\n\t]','',text)
     text = re.sub('[\s]+',' ',text)
     return text
+
 
 @app.route("/api/getIntention",methods=['POST'])
 def classifyText():
@@ -44,20 +53,32 @@ def classifyText():
     return jsonify({'intent':intent}) , 200
     
 
+def integrated_data():
+
+
+
 @app.route("/api/getRecommend", methods=["POST"])
 def recommend():
     text = json.loads(request.data.decode('utf-8'))['text']
-    
     tokenized_words = word_tokenize(cleanText(text))
-    results = model.docvecs.most_similar([model.infer_vector(tokenized_words)])
+    results = model.docvecs.most_similar([model.infer_vector(tokenized_words)], topn = 5)
     
-    res = dataset.iloc[[res[0][0] for res in results]]
-
+    select = np.zeros(dataset.shape[0], dtype=np.bool)
+    for r,p in results:
+        select[r] = True
+    
+    res = dataset[select]
+    res["description"] = res['description'].apply(lambda des : cleanText(des))
     obj = json.loads(res.to_json(orient='records', force_ascii=False))    
     return jsonify(obj), 200
 
-# @app.route("/api/getAsking", methods=["POST"])
-# def asking():
+
+@app.route("/api/getDescription", methods=["POST"])
+def describe():
+    title = json.loads(request.data.decode('utf-8'))['text']
+    data = integrated_data()
+    ret = json.loads(data[data["title"] == titile].to_json(orient='records', force_ascii=False))
+    return jsonify(ret), 200
 
 
 if __name__ == "__main__":
