f1e9e3356acf95655c7d859ad65275b47af418d9
ThipokKub <thipok30386@gmail.com>
2018-09-01 15:13:42 +0700

    Add modal wireframe

--- frontend/src/components/modal.js
+++ frontend/src/components/modal.js
@@ -12,20 +12,131 @@ const ModalStyled = styled.section`
     > div {
         width: 100%;
         background-color: #FFF;
+        border-radius: 5px;
         margin-bottom: 50px;
         /* height: 100%; */
         .modal-content {
             box-sizing: border-box;
             position: relative;
-            padding: 40px 10px 10px 10px;
+            padding: 10px 10px 10px 10px;
+            /* padding: 40px 10px 10px 10px; */
 
             width: 100%;
             /* height: 100%; */
         }
     }
+`;
+
+// const DebugTest = (props) => {
+//     return (
+//         <section>
+//             {Array.from(new Array(50).keys()).map((it) => <span key={it}>test {it}<br /></span>)}
+//         </section>
+//     );
+// }
+
+const FullDescriptionStyled = styled.section`
+    display: flex;
+    > div {
+        padding: 10px;
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
 `
 
-const Toggle = () => ModalStore.dispatch({ type: types.CLOSE })
+const FullDescription = (props) => {
+    const { title, amphoe, category, description, lat, lon, province, source, tambon, url } = props; 
+    return (
+        <FullDescriptionStyled>
+            <div>
+                <h2>{title}</h2>
+                <img src="https://via.placeholder.com/350x350" />
+            </div>
+            <div>
+                <p>{description}</p>
+                <p>
+                    <div>
+                        <b>ตำบล</b>&nbsp;{tambon}&nbsp;
+                        <b>อำเภอ</b>&nbsp;{amphoe}&nbsp;
+                        <b>จังหวัด</b>&nbsp;{province}&nbsp;
+                    </div>
+                    <div>
+                        <b>Latitude</b>&nbsp;{lat}&nbsp;
+                        <b>Longitude</b>&nbsp;{lon}
+                    </div>
+                </p>
+                <p>
+                    <b>ประเภท</b>&nbsp;{category}<br />
+                    <b>source</b>&nbsp;{source}
+                    <b>url</b>&nbsp;{url}
+                </p>
+            </div>
+        </FullDescriptionStyled>
+    );
+}
+
+const ListDescriptionStyled = styled.section`
+    display: flex;
+    flex-flow: row wrap; 
+
+    .col {
+        display: flex;
+        &.grow {
+            flex-grow: 1;
+        }
+
+        .list {
+            width: 400px;
+            max-width: 400px;
+            min-width: 400px;
+            height: 100%;
+            background-color: #888;
+        }
+    
+        .info {
+            width: 100%;
+            height: 100%;
+            background-color: #777;
+            flex-grow : 1;
+        }
+    }
+    /* > div {
+        padding: 10px;
+        box-sizing: border-box;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    } */
+`
+
+const ListDescription = (props) => {
+    return (
+        <ListDescriptionStyled>
+            <div className="col">
+                <section className="list">
+                    <article>
+                        ABC
+                    </article>
+                    <article>
+                        ABC
+                    </article>
+                    <article>
+                        ABC
+                    </article>
+                </section>
+            </div>
+            <div className="col grow">
+                <section className="info">
+                    Hello
+                </section>
+            </div>
+        </ListDescriptionStyled>
+    );
+}
+
+const Toggle = () => ModalStore.dispatch({ type: types.RESETnCLOSE })
 
 const Modal = (props) => {
     return (
@@ -42,11 +153,13 @@ const Modal = (props) => {
                             }}
                             onClick={Toggle}
                         />
-                        { Array.from(new Array(50).keys()).map((it) => <span key={it}>test {it}<br /></span>)}
+                        <ListDescription {...props.data} />
                     </article>
                 </OutsideClickHandler>
             </ModalStyled>
     )
 }
+
+//<FullDescription {...props.data} />
 
 export default Modal;
